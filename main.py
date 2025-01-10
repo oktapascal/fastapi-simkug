8b857318371827085ddfa9e832a7a7a0d217582c
oktapascal <oktaiscool@gmail.com>
2025-01-10 14:21:34 +0700

    update excel export buku besar

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from fastapi.responses import FileResponse
 from dotenv import load_dotenv
 from database import database
 from datetime import datetime as dt, datetime, timedelta
+from openpyxl import Workbook
+from openpyxl.styles import Alignment, Border, Side, PatternFill
 import os
 import time
 import platform
@@ -16,6 +18,7 @@ import pandas as pd
 import math
 import numpy as np
 import QuantLib as ql
+# import logging
 
 load_dotenv()
 
@@ -52,40 +55,195 @@ def test_db():
 @app.get('/api/excel/export/buku-besar')
 def excel_export_bukubesar(background_task: BackgroundTasks):
   try:
-    columns = ['NO BUKTI','NO DOKUMEN','TANGGAL','KODE PP','KETERANGAN','DEBET','KREDIT']
-
     t0 = time.perf_counter()
 
     dbproduk = connect_dbproduk()
-    cursor = dbproduk.cursor()
-
-    query = f'''
-    select a.no_bukti,a.no_dokumen,a.tanggal,a.kode_pp,a.keterangan,
-      case when a.dc='D' then a.nilai else 0 end as debet,case when a.dc='C' then a.nilai else 0 end as kredit
-    from gldt_h a
-    where a.kode_lokasi=? and periode=?
-    order by a.tanggal
-    '''
-    # params = ['51','1152007','202301']
-    params = ['51','202401']
-    # params = ['01', '2024']
-    cursor.execute(query, params)
+    list_bulan = ["JANUARI", "FEBRUARI", "MARET", "APRIL", "MEI", "JUNI", "JULI", "AGUSTUS", "SEPTEMBER", "OKTOBER",
+                  "NOVEMBER", "DESEMBER"]
+    periode = '202410'
+    # Mengambil tahun dan bulan
+    year = periode[:4]  # "2024"
+    month_index = int(periode[4:]) - 1  # Dikurangi 1 agar bulan dimulai dari indeks 0
 
     read_time = f'{time.perf_counter() - t0:.1f} seconds'
 
-    df = pd.DataFrame.from_records(cursor.fetchall(), columns=columns)
+    with dbproduk.cursor() as cursor:
+      query = f'''
+      select a.kode_akun, a.nama, a.so_awal
+      from glma_tmp a 
+      where a.periode = '{periode}' and a.kode_lokasi = '01'
+      order by a.kode_akun
+      '''
+
+      cursor.execute(query)
+
+      rows = cursor.fetchall()
+
+      # Dictionary untuk menyimpan data unik berdasarkan kode_akun
+      data_akun = {}
+
+      for kode_akun, nama_akun, saldo_awal in rows:
+        if kode_akun not in data_akun:
+          data_akun[kode_akun] = {
+            'kode_akun': kode_akun,
+            'nama_akun': nama_akun,
+            'fix_saldo': saldo_awal,
+            'saldo_awal': saldo_awal,
+            'total_debet': 0,
+            'total_kredit': 0,
+            'total_saldo': 0,
+            'jurnal': []
+          }
+
+      result = list(data_akun.values())
+
+      # Membuat string berisi kode akun
+      kode_akun_list = ','.join(f"'{kode}'" for kode in data_akun.keys())
+
+      query = f'''
+      select a.no_bukti, a.no_dokumen, convert(varchar, a.tanggal, 103) tanggal, a.keterangan, a.kode_akun,
+       case when a.dc='D' then nilai else 0 end as debet,
+       case when a.dc='C' then nilai else 0 end as kredit
+      from (
+        select a.kode_lokasi, a.no_bukti, a.no_dokumen, a.periode, a.tanggal, a.kode_akun, a.kode_pp, a.kode_drk, a.dc,
+        a.nilai, a.keterangan, a.modul
+	      from gldt_h a
+	      where a.kode_lokasi='01' and substring(a.periode,1,4)=substring('{periode}',1,4) and a.kode_akun in ({kode_akun_list}) and a.periode = '{periode}'
+	      union all
+	      select a.kode_lokasi, a.no_bukti, a.no_dokumen, a.periode, a.tanggal, a.kode_akun, a.kode_pp, a.kode_drk, a.dc,
+	      a.nilai, a.keterangan, a.modul
+	      from gldt a
+	      where a.kode_lokasi='01' and substring(a.periode,1,4)=substring('{periode}',1,4) and a.kode_akun in ({kode_akun_list}) and a.periode = '{periode}'
+      ) a
+      order by a.tanggal
+      '''
+
+      cursor.execute(query)
+
+      rows = cursor.fetchall()
+
+      for no_bukti, no_dokumen, tanggal, keterangan, kode_akun, debet, kredit in rows:
+        if kode_akun in data_akun:
+          data_akun[kode_akun]['saldo_awal'] += debet - kredit
+          data_akun[kode_akun]['total_saldo'] = data_akun[kode_akun]['saldo_awal']
+          data_akun[kode_akun]['total_debet'] += debet
+          data_akun[kode_akun]['total_kredit'] += kredit
+
+          data_akun[kode_akun]['jurnal'].append({
+            'no_bukti': no_bukti,
+            'no_dokumen': no_dokumen,
+            'tanggal': tanggal,
+            'keterangan': keterangan,
+            'debet': debet,
+            'kredit': kredit,
+            'saldo': data_akun[kode_akun]['total_saldo']
+          })
+
+      result = list(data_akun.values())
+
+    workbook = Workbook()
+    sheet = workbook.active
+    sheet.title = 'Laporan Buku Besar'
+
+    start_row = 1
+    sheet.merge_cells(start_row=start_row, end_row=start_row, start_column=1, end_column=7)
+    sheet.cell(row=start_row, column=1).value = 'PT. GRAHA INFORMATIKA NUSANTARA'
+    sheet.cell(row=start_row, column=1).alignment = Alignment(horizontal='center')
+
+    sheet.merge_cells(start_row=start_row + 1, end_row=start_row + 1, start_column=1, end_column=7)
+    sheet.cell(row=start_row + 1, column=1).value = 'LAPORAN BUKU BESAR'
+    sheet.cell(row=start_row + 1, column=1).alignment = Alignment(horizontal='center')
+
+    sheet.merge_cells(start_row=start_row + 2, end_row=start_row + 2, start_column=1, end_column=7)
+    sheet.cell(row=start_row + 2, column=1).value = f'{list_bulan[month_index]} {year}'
+    sheet.cell(row=start_row + 2, column=1).alignment = Alignment(horizontal='center')
+
+    sheet.column_dimensions["A"].width = 19.55
+    sheet.column_dimensions["B"].width = 19.18
+    sheet.column_dimensions["C"].width = 10.20
+    sheet.column_dimensions["D"].width = 34.27
+    sheet.column_dimensions["E"].width = 10.27
+    sheet.column_dimensions["F"].width = 8.27
+    sheet.column_dimensions["G"].width = 10.27
+
+    for value in result:
+      sheet.cell(row=start_row + 4, column=1).value = 'KODE AKUN'
+      sheet.cell(row=start_row + 4, column=2).value = f": {value['kode_akun']}"
+      sheet.cell(row=start_row + 4, column=1).border = Border(left=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'))
+      sheet.cell(row=start_row + 4, column=2).border = Border(right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'))
+      sheet.merge_cells(start_row=start_row + 4, end_row=start_row + 4, start_column=2, end_column=7)
+
+      sheet.cell(row=start_row + 5, column=1).value = 'NAMA AKUN'
+      sheet.cell(row=start_row + 5, column=2).value = f": {value['nama_akun']}"
+      sheet.cell(row=start_row + 5, column=1).border = Border(left=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+      sheet.cell(row=start_row + 5, column=2).border = Border(right=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+      sheet.merge_cells(start_row=start_row + 5, end_row=start_row + 5, start_column=2, end_column=7)
+
+      sheet.cell(row=start_row + 6, column=1).value = 'SALDO AWAL'
+      sheet.cell(row=start_row + 6, column=1).alignment = Alignment(horizontal='right')
+      sheet.cell(row=start_row + 6, column=7).value = value['fix_saldo']
+      sheet.cell(row=start_row + 6, column=7).number_format = '#,##0'
+      sheet.cell(row=start_row + 6, column=7).alignment = Alignment(horizontal='right')
+      sheet.cell(row=start_row + 6, column=1).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+      sheet.cell(row=start_row + 6, column=7).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+      sheet.merge_cells(start_row=start_row + 6, end_row=start_row + 6, start_column=1, end_column=6)
+
+      jurnal_header = ['NO BUKTI', 'NO DOKUMEN', 'TANGGAL', 'KETERANGAN', 'DEBET', 'KREDIT', 'BALANCE']
+      for col, header in enumerate(jurnal_header, start=1):
+        sheet.cell(row=start_row + 7, column=col).value = header
+        sheet.cell(row=start_row + 7, column=col).fill = PatternFill(start_color='B5B5B5', end_color='B5B5B5', fill_type='solid')
+        sheet.cell(row=start_row + 7, column=col).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+        sheet.cell(row=start_row + 7, column=col).alignment = Alignment(horizontal='center')
+
+      current_row = start_row + 7 + 1
+      for jurnal in value['jurnal']:
+        sheet.cell(row=current_row, column=1).value = jurnal['no_bukti']
+        sheet.cell(row=current_row, column=1).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+        sheet.cell(row=current_row, column=2).value = jurnal['no_dokumen']
+        sheet.cell(row=current_row, column=2).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+        sheet.cell(row=current_row, column=3).value = jurnal['tanggal']
+        sheet.cell(row=current_row, column=3).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+        sheet.cell(row=current_row, column=4).value = jurnal['keterangan']
+        sheet.cell(row=current_row, column=4).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+        sheet.cell(row=current_row, column=5).value = jurnal['debet']
+        sheet.cell(row=current_row, column=5).number_format = '#,##0'
+        sheet.cell(row=current_row, column=5).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+        sheet.cell(row=current_row, column=6).value = jurnal['kredit']
+        sheet.cell(row=current_row, column=6).number_format = '#,##0'
+        sheet.cell(row=current_row, column=6).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+        sheet.cell(row=current_row, column=7).value = jurnal['saldo']
+        sheet.cell(row=current_row, column=7).number_format = '#,##0'
+        sheet.cell(row=current_row, column=7).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+        current_row += 1
+
+      sheet.cell(row=current_row, column=1).value = 'TOTAL'
+      sheet.cell(row=current_row, column=1).alignment = Alignment(horizontal='right')
+      sheet.cell(row=current_row, column=1).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+      sheet.merge_cells(start_row=current_row, end_row=current_row, start_column=1, end_column=4)
+
+      sheet.cell(row=current_row, column=5).value = value['total_debet']
+      sheet.cell(row=current_row, column=5).number_format = '#,##0'
+      sheet.cell(row=current_row, column=5).alignment = Alignment(horizontal='right')
+      sheet.cell(row=current_row, column=5).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+
+      sheet.cell(row=current_row, column=6).value = value['total_kredit']
+      sheet.cell(row=current_row, column=6).number_format = '#,##0'
+      sheet.cell(row=current_row, column=6).alignment = Alignment(horizontal='right')
+      sheet.cell(row=current_row, column=6).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+
+      sheet.cell(row=current_row, column=7).value = value['total_saldo']
+      sheet.cell(row=current_row, column=7).number_format = '#,##0'
+      sheet.cell(row=current_row, column=7).alignment = Alignment(horizontal='right')
+      sheet.cell(row=current_row, column=7).border = Border(left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'), top=Side(style='thin', color='000000'), bottom=Side(style='thin', color='000000'))
+
+      start_row = start_row + 4 + len(value['jurnal']) + 3
 
     today = dt.now()
     unique_id = today.strftime('%Y%m%d%H%M%S')
 
-    file_name = f'DATA_GL_{unique_id}.xlsx'
+    file_name = f'BUKU_BESAR_{list_bulan[month_index]}{year}_{unique_id}.xlsx'
 
-    writer = pd.ExcelWriter(file_name)
-    df.to_excel(writer, index=False)
-
-    writer.close()
-    cursor.close()
-    dbproduk.close()
+    workbook.save(file_name)
 
     execution_time = f'{time.perf_counter() - t0:.1f} seconds'
 
@@ -115,7 +273,10 @@ def excel_export_bukubesar(background_task: BackgroundTasks):
 
     return FileResponse(path=file_name, headers=headerResponse, filename=file_name)
   except Exception as ex:
+    # logging.exception("An exception was thrown!")
     return {'status': 'ERROR', 'message': str(ex)}
+  finally:
+    dbproduk.close()
 
 # @app.post('/api/calculate-bond')
 # def calculate_bond(rate_coupon: float = Form(), rate_yield: float = Form(), frequency_count: int = Form(), basis: int = Form(), nominal: float = Form(), settlement_date: str = Form(), maturity_date: str = Form()):
